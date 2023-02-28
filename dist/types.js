db598b255115b8364a4d5a20181d2ab3d3d83c47
Shvyrkaloff <shvyrkalovm@mail.ru>
2023-02-28 18:32:52 +0300

    Initial commit

---
+++ dist/types.js
@@ -0,0 +1,9 @@
+var Human = /** @class */ (function () {
+    function Human(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return Human;
+}());
+var user = new Human('Ben', 30);
+console.log(user);
